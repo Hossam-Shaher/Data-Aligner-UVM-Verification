256f4acfb68ac944972e270a2926a4aa3c2ae44d
Hossam Shaher <hosamshaher43@gmail.com>
2026-01-24 19:24:53 +0200

    Update apb_coverage_collector.sv

--- TB/apb_coverage_collector.sv
+++ TB/apb_coverage_collector.sv
@@ -11,7 +11,8 @@
     algn_reg_block 		reg_block;
     //One responsibility of this coverage collector is to collect register-level functional coverage
     //so it needs a handle to the register model
-
+    
+/*
     covergroup cover_item;
         option.per_instance = 1;
 
@@ -47,7 +48,7 @@
           (APB_READ, APB_WRITE => APB_READ, APB_WRITE);
         }
     endgroup: cover_item
-
+*/
     covergroup cover_reg;
       option.per_instance = 1;
 
@@ -58,7 +59,7 @@
 
       CTRL_SIZE : coverpoint reg_block.CTRL.SIZE.value {
         option.comment = "Value of CTRL.SIZE";
-        bins values[]  = {[1:4]};
+        bins values[]  = {1, 2, 4};
       }
 
       CTRL_OFFSET_x_CTRL_SIZE : cross CTRL_OFFSET, CTRL_SIZE {
@@ -67,6 +68,7 @@
                                   (binsof(CTRL_OFFSET) intersect {2} && binsof(CTRL_SIZE) intersect {3, 4})    || 
                                   (binsof(CTRL_OFFSET) intersect {3} && binsof(CTRL_SIZE) intersect {2, 3, 4});
       }
+      
       /*
       IRQ_RX_FIFO_EMPTY : coverpoint reg_block.IRQ.RX_FIFO_EMPTY.value[0] {
         option.comment = "Value of IRQ.RX_FIFO_EMPTY";
@@ -85,6 +87,10 @@
       }
       */
 
+      IRQ_MAX_DROP : coverpoint reg_block.IRQ.MAX_DROP.value[0] {
+        option.comment = "Value of IRQ.MAX_DROP";
+      }
+      
       //Note that the data type of value is uvm_reg_data_t, so its size is not the size of the register field, 
       //that's why we used 
       //    bins values[]  = { . . . };
@@ -94,26 +100,26 @@
 
     function new(string name, uvm_component parent);
       super.new(name, parent);
-      cover_item = new();
+      /* cover_item = new(); */
       cover_reg = new();  
     endfunction: new  
 
     function void write(apb_seq_item_mon t);
       item = t;
-      cover_item.sample();
+      /* cover_item.sample(); */
       cover_reg.sample();
     endfunction: write
 
     function string coverage2string();
       coverage2string  = {
-        $sformatf("\n  cover_item:                    %.2f%%", cover_item.get_inst_coverage()),
+      /*$sformatf("\n  cover_item:                    %.2f%%", cover_item.get_inst_coverage()),
         $sformatf("\n       direction:                %.2f%%", cover_item.direction.get_inst_coverage()),
         $sformatf("\n       trans_direction:          %.2f%%", cover_item.trans_direction.get_inst_coverage()),
         $sformatf("\n       response:                 %.2f%%", cover_item.response.get_inst_coverage()),
         $sformatf("\n       response_x_direction:     %.2f%%", cover_item.response_x_direction.get_inst_coverage()),
         $sformatf("\n       length:                   %.2f%%", cover_item.length.get_inst_coverage()),
-        $sformatf("\n       prev_item_delay:          %.2f%%", cover_item.prev_item_delay.get_inst_coverage()),
-        $sformatf("\n  cover_reg:                 	  %.2f%%", cover_reg.get_inst_coverage()),
+        $sformatf("\n       prev_item_delay:          %.2f%%", cover_item.prev_item_delay.get_inst_coverage()), */
+		    $sformatf("\n  cover_reg:                 	  %.2f%%", cover_reg.get_inst_coverage()),
         $sformatf("\n       CTRL_OFFSET:              %.2f%%", cover_reg.CTRL_OFFSET.get_inst_coverage()),
         $sformatf("\n       CTRL_SIZE:                %.2f%%", cover_reg.CTRL_SIZE.get_inst_coverage()),
         $sformatf("\n       CTRL_OFFSET_x_CTRL_SIZE:  %.2f%%", cover_reg.CTRL_OFFSET_x_CTRL_SIZE.get_inst_coverage()),
@@ -121,11 +127,12 @@
         $sformatf("\n       IRQ_RX_FIFO_FULL:         %.2f%%", cover_reg.IRQ_RX_FIFO_FULL.get_inst_coverage()),
         $sformatf("\n       IRQ_TX_FIFO_EMPTY:        %.2f%%", cover_reg.IRQ_TX_FIFO_EMPTY.get_inst_coverage()),
         $sformatf("\n       IRQ_TX_FIFO_FULL:         %.2f%%", cover_reg.IRQ_TX_FIFO_FULL.get_inst_coverage()),	*/
+        $sformatf("\n       IRQ_MAX_DROP:             %.2f%%", cover_reg.IRQ_MAX_DROP.get_inst_coverage()),       
         $sformatf("\n  ======================================"),
         $sformatf("\n  OVERALL Coverage:              %.2f%%", $get_coverage())
 
         };    
-    endfunction
+    endfunction: coverage2string
 
     function void report_phase(uvm_phase phase);
       `uvm_info("COVERAGE", coverage2string(), UVM_NONE)
