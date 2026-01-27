bf9991061c64a21d98f31818830070922e29632f
Hossam Shaher <hosamshaher43@gmail.com>
2026-01-27 06:37:48 +0200

    Update algn_test_base.sv

--- TB/algn_test_base.sv
+++ TB/algn_test_base.sv
@@ -8,7 +8,7 @@
 
     `uvm_component_utils(algn_test_base)
 
-    algn_env 		m_algn_env;
+    algn_env 		    m_algn_env;
     algn_env_config	m_algn_env_config;
 
     function new(string name, uvm_component parent);
@@ -17,23 +17,23 @@
 
     function void build_phase(uvm_phase phase);
       super.build_phase(phase);
-      m_algn_env 		= algn_env::type_id::create("m_algn_env", this); 
+      m_algn_env 		    = algn_env::type_id::create("m_algn_env", this); 
       m_algn_env_config = algn_env_config::type_id::create("m_algn_env_config", this);  
 
       if( ! uvm_config_db#(virtual apb_if)::get(this, "", "apb_vif", m_algn_env_config.m_apb_agent_config.apb_vif) ) begin
-        `uvm_error(this.get_full_name(), "apb_vif NOT found");
+        `uvm_error(this.get_full_name(), "apb_vif NOT found")
       end
 
       if( ! uvm_config_db#(virtual md_if#(32))::get(this, "", "md_rx_vif", m_algn_env_config.m_md_agent_config_master.md_vif) ) begin
-        `uvm_error(this.get_full_name(), "md_vif (rx) NOT found");
+        `uvm_error(this.get_full_name(), "md_vif (rx) NOT found")
       end
 
       if( ! uvm_config_db#(virtual md_if#(32))::get(this, "", "md_tx_vif", m_algn_env_config.m_md_agent_config_slave.md_vif) ) begin
-        `uvm_error(this.get_full_name(), "md_vif (tx) NOT found");
+        `uvm_error(this.get_full_name(), "md_vif (tx) NOT found")
       end
 
       if( ! uvm_config_db#(virtual algn_if)::get(this, "", "algn_vif", m_algn_env_config.algn_vif) ) begin
-        `uvm_error(this.get_full_name(), "algn_vif NOT found");
+        `uvm_error(this.get_full_name(), "algn_vif NOT found")
       end
 
       uvm_config_db#(algn_env_config)::set(this, "m_algn_env", "m_algn_env_config", m_algn_env_config);
